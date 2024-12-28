c6105ad682b9435e72521ab8262bc56a56ca8520
João Paulo Maciel Borges <joaopaulomacielborges@gmail.com>
2024-12-28 15:17:04 -0300

    adicionando calculadora de calorias

--- app.py
+++ app.py
@@ -20,5 +20,22 @@ def calcular_imc():
     
     return render_template("calculadora_imc.html", imc=imc)
 
+@app.route("/calcular_calorias", methods=["GET", "POST"])
+def calcular_calorias():
+    calorias = None
+    taxa_basal = None
+    if request.method == "POST":
+        peso = float(request.form.get("peso"))
+        altura = float(request.form.get("altura"))
+        idade = float(request.form.get("idade"))
+
+        if not peso or not altura or not idade:
+            return render_template("calculadora_calorias.html", erro="Forneça peso, altura e idade")
+
+        taxa_basal = round(88.36 + (13.4 * peso) + (4.8 * altura) - (5.7 * idade))
+    
+    return render_template("calculadora_calorias.html", taxa_basal=taxa_basal)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
